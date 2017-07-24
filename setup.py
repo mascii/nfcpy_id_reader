6d49436762eba2ee456734c9ea6d24ba7341d086
Masaki Koyanagi <mascii@gmail.com>
2017-07-25 00:42:19 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ requires = ['nfcpy']
 
 setup(
     name='nfcpy_id_reader',
-    version='0.0.3',
+    version='0.1.0',
     description='Read the ID of an NFC Tag with nfcpy',
     url='https://github.com/mascii/nfcpy_id_reader',
     author='Masaki Koyanagi',
-    author_email='mascii@gmail',
+    author_email='mascii@gmail.com',
     license='EUPL 1.1',
     keywords='nfc pasori',
     packages=['nfcpy_id_reader'],
